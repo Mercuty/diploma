d6495b5f152d536ba157b1d24b235d5329f449d3
Mercuty <a-zoshchuk@yandex.ru>
2018-02-20 01:10:22 +0300

    add - clustering algorythm (not working)

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+from random import randint
 from time import sleep
 
 import numpy as np
 import vk
-from random import randint
 from igraph import Graph, plot
 
 from tok import my_token
 
-researched_id = 13221877
+researched_id = 53523636
 
 
 def initialisation():  # инициализация для вк апи
@@ -34,13 +34,13 @@ def friends_clusterising(mutural_list):
         friends_common[i][0] = friend  # id друга
         friends_common[i][1] = len(mutural_list[friend])  # количество общих друзей с юзером
         friends_common[i][2] = ''  # номер кластера
-        friends_common[i][3] = '' # цвет кластера
-        friends_common[i][4] = mutural_list[friend] # id общих друзей
+        friends_common[i][3] = ''  # цвет кластера
+        friends_common[i][4] = mutural_list[friend]  # id общих друзей
         i += 1
-    friends_common.sort(key=lambda friends: friends[1]) # сортируем по возрастанию количества общих друзей
+    friends_common.sort(key=lambda friends: friends[1])  # сортируем по возрастанию количества общих друзей
     cluster_num = 0
     i = 0
-    for friend in friends_common: #первоначальное распределение в кластера
+    for friend in friends_common:  # первоначальное распределение в кластера
         if friend[2] == '':
             friend[2] = cluster_num
             for common_friend in mutural_list[friend[0]]:
@@ -48,7 +48,7 @@ def friends_clusterising(mutural_list):
                     if ((friends_common[j][0] == common_friend) & (friends_common[j][2] == '')):
                         friends_common[j][2] = cluster_num
             cluster_num += 1
-    for i in range(500):  # калибруем кластера
+    for i in range(mutural_list_col):  # калибруем кластера
         for friend in friends_common:
             common_friends_clusters = np.zeros(cluster_num)
             people_in_cluster = np.zeros(cluster_num)
@@ -62,12 +62,28 @@ def friends_clusterising(mutural_list):
                 if people_in_cluster[iter] != 0:
                     common_friends_clusters[iter] /= people_in_cluster[iter]
             friend[2] = common_friends_clusters.argmax()
-            for cluster in range(cluster_num): # присваеваем каждому кластеру рандомный цвет
-                cluster_color.append(str(randint(0, 255)) + ", " + str(randint(0, 255)) + ", " + str(randint(0, 255)))
-            for friend in friends_common: # передаем каждому юзеру цвет его кластера
-                friend[3] = cluster_color[friend[2]]
-                friends_color.append(cluster_color[friend[2]])
-            friends_color.append("255, 255, 255")
+    for i in range(10):
+        for cluster in range(cluster_num):
+            cluster_col = [0] * cluster_num
+            for friend in friends_common:  # считаем количество друзей друга в разных кластерах
+                if friend[2] == cluster:
+                    for m_friend in friend[4]:
+                        for friend_1 in friends_common:
+                            if friend_1[0] == m_friend:
+                                cluster_col[friend_1[2]] += 1
+            for cluster_c in range(cluster_num):
+                if (cluster_col[cluster_c] != 0) & (cluster_col[cluster] != 0) & (cluster != cluster_c):
+                    if cluster_col[cluster_c] / cluster_col[cluster] > 0.5:
+                        for i in range(len(friends_common)):
+                            if friends_common[i][2] == cluster_c:
+                                friends_common[i][2] == 0
+    cluster_color.append("255,255,255")
+    for cluster in range(cluster_num):  # присваеваем каждому кластеру рандомный цвет
+        cluster_color.append(str(randint(0, 255)) + ", " + str(randint(0, 255)) + ", " + str(randint(0, 255)))
+    for friend in friends_common:  # передаем каждому юзеру цвет его кластера
+        friend[3] = cluster_color[friend[2]]
+        friends_color.append(cluster_color[friend[2]])
+    friends_color.append("255, 255, 255")
     plot_graf(friends_common, mutural_list, friends_color)
 
 
@@ -76,31 +92,31 @@ def plot_graf(friends_common, mutural_list, friends_color):
     ids_in_string = []
     g = Graph()
     nodes_col = len(friends_common)
-    g.add_vertices(nodes_col + 1) # +1 для самого пользователя
+    g.add_vertices(nodes_col + 1)  # +1 для самого пользователя
     for friend in friends_common:
         ids_in_string.append(str(friend[0]))
     ids_in_string.append(str(researched_id))
-    g.vs["name"] = ids_in_string #называем вершины графа айдишниками юзеров
-    g.vs["color"] = friends_color #красим вершины в соответствии с кластером юзера
+    g.vs["name"] = ids_in_string  # называем вершины графа айдишниками юзеров
+    g.vs["color"] = friends_color  # красим вершины в соответствии с кластером юзера
     user_index = g.vs.find(str(researched_id)).index
-    for friend in mutural_list: #строим связи между вершинами
+    for friend in mutural_list:  # строим связи между вершинами
         friend_node = g.vs.find(str(friend))
-        g.add_edges([(friend_node.index, user_index)]) # связываем юзера с исследуемым пользователем
-        for common_friend in mutural_list[friend]: # связываем каждого юзера с общими друзьями
+        g.add_edges([(friend_node.index, user_index)])  # связываем юзера с исследуемым пользователем
+        for common_friend in mutural_list[friend]:  # связываем каждого юзера с общими друзьями
             try:
                 common_friend_node = g.vs.find(str(common_friend))
                 connections = g.es.select(_within=(friend_node.index, common_friend_node.index))
-                try: # проверяем, не добавляли ли мы уже эту связь
+                try:  # проверяем, не добавляли ли мы уже эту связь
                     a = (connections[0])
                     # print("connection exists")
-                except: # если не добавляли, то добавляем
+                except:  # если не добавляли, то добавляем
                     g.add_edges([(friend_node.index, common_friend_node.index)])
             except Exception as e:
                 print(e)
     g.vs["label"] = g.vs["name"]
     print(len(ids_in_string))
     layout = g.layout("kk")
-    plot(g, "social_network.png") # сохраняем картинку графа
+    plot(g, "social_network.png", margin=0)  # сохраняем картинку графа
     # plot(g, layout=layout, margin=10)
 
 
@@ -108,19 +124,20 @@ def main():
     api = initialisation()
     my_friends = get_friends(api, researched_id)
     print(my_friends["count"])
-    couples_of_friends = int(my_friends["count"] / 20) # ходим в апи пачками по 20 человек, чтобы в случае ошибки не было больно
+    couples_of_friends = int(
+        my_friends["count"] / 20)  # ходим в апи пачками по 20 человек, чтобы в случае ошибки не было больно
     my_friends = my_friends["items"]
     print(couples_of_friends)
-    mutural_list = {} # словарь [юзер]:[общий друг1, общий друг2 ...]
+    mutural_list = {}  # словарь [юзер]:[общий друг1, общий друг2 ...]
     for i in range(couples_of_friends + 1):
         sleep(0.25)
         slice = my_friends[i * 20:(i + 1) * 20]
-        try: # идем за информацией о пачке юзеров в апи, если ошибка не венулась, аве нам
+        try:  # идем за информацией о пачке юзеров в апи, если ошибка не венулась, аве нам
             mutural = api.friends.getMutual(source_uid=researched_id, target_uids=slice)
             print("ok " + str(i))
             for friend in mutural:
                 mutural_list[friend["id"]] = friend["common_friends"]
-        except: # если вернулась ошибка, то идем в апи за каждым юзером в отдельности
+        except:  # если вернулась ошибка, то идем в апи за каждым юзером в отдельности
             print("error API " + str(i))
             for k in range(len(slice)):
                 try:
